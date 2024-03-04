5b0b8021a1bfaf8145b65c9356bf0ec1c4a28c0c
Gustavo Pereira <gugarosp@gmail.com>
2024-03-04 15:19:13 -0300

    Modals buttons events added

---
+++ js/modals.js
@@ -0,0 +1,19 @@
+const modals = document.querySelector("#modals")
+const landscapeWarning = document.querySelector("#landscape-warning");
+const soundWarning = document.querySelector("#sound-warning");
+
+setTimeout(() => {
+    landscapeWarning.classList.add("show-modal-container");
+}, 1);
+
+landscapeWarning.querySelector("button").addEventListener("click", () => {
+    landscapeWarning.classList.remove("show-modal-container");
+    soundWarning.classList.add("show-modal-container");
+})
+
+soundWarning.querySelectorAll("button").forEach(element => {
+    element.addEventListener("click", event => {
+        modals.classList.add("hide");
+    });
+})
+
